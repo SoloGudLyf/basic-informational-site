4a036d4aa906835a60e57fb4ce452257bd27d700
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-15 02:00:59 +0000

    feat: Handle routing of pages and place content on error page
    To navigate users to accurate pages and also show error
    message if page not found

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 import http from "node:http";
 import fs from "node:fs";
-
-
-// Read data from index.html
-let homePage
-try {
- homePage = fs.readFileSync("index.html", "utf8");
-} catch (error) {
-  console.error(error);
-}
+import url from "node:url";
 
 // Create a local server to receive data from
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(homePage);
+  const pathName = url.parse(req.url).pathname;
+
+  switch (pathName) {
+    case "/about.html":
+      let aboutPage;
+      try {
+        aboutPage = fs.readFileSync("about.html", "utf8");
+      } catch (error) {
+        console.error(error);
+      }
+      res.write(aboutPage);
+      break;
+    case "/contact-me.html":
+      let contactPage;
+      try {
+        contactPage = fs.readFileSync("contact-me.html", "utf8");
+      } catch (error) {
+        console.error(error);
+      }
+      res.write(contactPage);
+      break;
+    case "/index.html":
+      let homePage;
+      try {
+        homePage = fs.readFileSync("index.html", "utf8");
+      } catch (error) {
+        console.error(error);
+      }
+      res.write(homePage);
+      break;
+    default:
+      let errorPage;
+      try {
+        errorPage = fs.readFileSync("404.html", "utf8");
+      } catch (error) {
+        console.error(error);
+      }
+      res.write(errorPage);
+  }
+
   res.end();
 });
 
