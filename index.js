42786b52efc1f507ece391ac235da691d9efce7c
SoloGudLyf <solomonteiko0@gmail.com>
2025-12-15 03:59:14 +0000

    feat: Configure execution of css files
    Enable server to load css file when requested and
    also load external links in a new tab with a safety
    measure in place.

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 import http from "node:http";
 import fs from "node:fs";
 import url from "node:url";
+import path from "node:path";
 
-// Create a local server to receive data from
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  const pathName = url.parse(req.url).pathname;
-
-  switch (pathName) {
-    case "/about.html":
-      let aboutPage;
-      try {
-        aboutPage = fs.readFileSync("about.html", "utf8");
-      } catch (error) {
-        console.error(error);
-      }
-      res.write(aboutPage);
-      break;
-    case "/contact-me.html":
-      let contactPage;
-      try {
-        contactPage = fs.readFileSync("contact-me.html", "utf8");
-      } catch (error) {
-        console.error(error);
-      }
-      res.write(contactPage);
-      break;
-    case "/index.html":
-      let homePage;
-      try {
-        homePage = fs.readFileSync("index.html", "utf8");
-      } catch (error) {
-        console.error(error);
-      }
-      res.write(homePage);
-      break;
-    default:
-      let errorPage;
-      try {
-        errorPage = fs.readFileSync("404.html", "utf8");
-      } catch (error) {
-        console.error(error);
-      }
-      res.write(errorPage);
+  // Read requested css file
+  if (req.url.match(".css$")) {
+    fs.readFile("style.css", function (err, data) {
+      if (err) console.error(err);
+      res.write(data);
+      res.end();
+    });
   }
 
-  res.end();
+  const contentType = getContentType(req.url);
+
+  res.writeHead(200, { "Content-Type": contentType });
+  const pathName = url.parse(req.url).pathname;
+
+  // Read requested file
+  fs.readFile(pathName.slice(1), function (err, data) {
+    if (err) console.error(err);
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(8080);
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case ".html":
+      return "text/html";
+    case ".css":
+      return "text/css";
+    case ".js":
+      return "text/javascript";
+    case ".jpg":
+    case ".jpeg":
+      return "image/jpeg";
+    case ".png":
+      return "image/png";
+    case ".gif":
+      return "image/gif";
+    default:
+      return "text/html";
+  }
+}
